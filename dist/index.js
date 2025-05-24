5293e271472f33f33f6937e8dd73e8633f549dc9
leander006 <leanderdsilva06@gmail.com>
2025-05-24 15:30:44 +0530

    done with socket and web rtc logic

--- dist/index.js
+++ dist/index.js
@@ -23,7 +23,6 @@ const io = new socket_io_1.Server(server, {
 io.on('connection', (socket) => {
     console.log('A user connected ', socket.id);
     socket.on('join-room', ({ username, roomId }) => {
-        // console.log(`${username} joined room: ${roomId}`);
         if (!allusers[roomId]) {
             allusers[roomId] = [];
         }
@@ -32,113 +31,64 @@ io.on('connection', (socket) => {
             userIdToSocket.set(socket.id, roomId);
         }
         socket.join(roomId);
-        // console.log(allusers[roomId]," ",userIdToSocket);
         for (const user of allusers[roomId]) {
-            // console.log(`User ${user.username} and `,username," and ",(user.username !== username));
             if (user.username !== username) {
                 socket.to(user.id).emit('user-joined', { username, id: socket.id });
-                // console.log(`User ${username} joined the room.`,roomId);
-                socket.emit('user-exist', { username: user.username, id: user.id });
-                // console.log(`User ${user.username} exist the room.`,roomId);
             }
         }
     });
-    socket.on('user:call', ({ to, offer }) => {
-        console.log(" user incoming call ", offer);
-        io.to(to).emit("incoming:call", { from: socket.id, offer });
+    socket.on('user:offer', ({ to, offer }) => {
+        console.log(" user incoming call ");
+        io.to(to).emit("incoming:offer", { from: socket.id, offer });
     });
     socket.on('user:answer', ({ to, answer }) => {
-        console.log(" user answering call  ", answer);
+        console.log(" user answering call  ");
         io.to(to).emit("incoming:answer", { from: socket.id, answer });
     });
-    // socket.on('create-room', ({username,roomId}) => {
-    //     console.log(`${username} created room: ${roomId}`);
-    //     // Add the user to the room's user list
-    //     if (!allusers[roomId]) {
-    //         allusers[roomId] = {};
-    //     }
-    //     allusers[roomId][username] = {id: socket.id };
-    //     console.log(allusers[roomId]);
-    //     // Filter out the username from the room's user list
-    //     io.emit("created", username);
-    // });
-    // socket.on('join-room', ({username,roomId}) => {
-    //     console.log(`${username} joined room: ${roomId}`);
-    //     if (!allusers[roomId]) {
-    //         allusers[roomId] = {};
-    //     }
-    //     allusers[roomId][username] = {id: socket.id };
-    //     console.log(allusers[roomId]);
-    //     const filteredUsers = Object.keys(allusers[roomId])
-    //     .filter(user => user !== username)
-    //     .reduce((acc, user) => {
-    //     acc[user] = allusers[roomId][user];
-    //     return acc;
-    //     }, {} as { [key: string]: { id: string } });
-    // console.log('Filtered users:', filteredUsers);
-    //     allusers[roomId]
-    //     if(Object.keys(filteredUsers).length > 0){
-    //         socket.emit('filtered-users', filteredUsers);
-    //         for(const user in filteredUsers){
-    //             io.to(filteredUsers[user].id).emit("joined", username);
-    //             console.log(`User ${username} joined the room.`,filteredUsers[user].id);
-    //         }        
-    //     }
-    // });
-    // socket.on("offer", ({from, to, offer,roomId}) => {
-    //     console.log({from , to ,roomId},"all users data ",allusers[roomId][to].id);
-    //     io.to(allusers[roomId][to].id).emit("offer", {from, to, offer});
-    // });
-    // socket.on("answer", ({from, to, answer,roomId}) => {
-    //     console.log({from , to ,roomId},"all users data ",allusers[roomId][from].id);
-    //    io.to(allusers[roomId][from].id).emit("answer", {from, to, answer});
-    // });
-    // socket.on("end-call", ({from, to,roomId}) => {
-    //     for(const user of allusers[roomId]) {
-    //         console.log(`User ${user.username} and `,from," and ",(user.username !== from));
-    //         if(user.username !== from) {
-    //             socket.to(user.id).emit('call-ended', {from, to});
-    //             console.log(`User ${from} ended the call.`,roomId);
-    //             socket.emit('call-ended', {from, to});
-    //             console.log(`User ${to} ended the call.`,roomId);
-    //         }
-    //     }
-    //     allusers[roomId].filter(user => user.username !== from);
-    //     console.log(allusers[roomId]);
-    //     // io.to(allusers[roomId][to].id).emit("end-call", {from, to});
-    // });
-    // socket.on("call-ended", caller => {
-    //     const [from, to,roomId] = caller;
-    //     io.to(allusers[roomId][from].id).emit("call-ended", caller);
-    //     io.to(allusers[roomId][to].id).emit("call-ended", caller);
-    // })
-    // socket.on("icecandidate", candidate => {
-    //     console.log({ candidate });
-    //     //broadcast to other peers
-    //     socket.broadcast.emit("icecandidate", candidate);
-    // }); 
+    socket.on('ice-candidate', ({ to, candidate }) => {
+        if (candidate !== null) {
+            io.to(to).emit("incoming:candidate", { from: socket.id, candidate });
+        }
+    });
+    socket.on('user:leave', ({ username, roomId }) => {
+        var _a;
+        const index = allusers[roomId].findIndex(user => user.username === username);
+        const to = (_a = allusers[roomId].find(user => user.username !== username)) === null || _a === void 0 ? void 0 : _a.id;
+        console.log(`User ${username} left room: ${roomId}`, to);
+        if (index !== -1) {
+            allusers[roomId].splice(index, 1);
+            socket.leave(roomId);
+            userIdToSocket.delete(socket.id);
+            if (to) {
+                socket.to(to).emit('user-left', { username, id: socket.id });
+            }
+        }
+        if (allusers[roomId].length === 0) {
+            delete allusers[roomId];
+        }
+    });
     socket.on('disconnect', () => {
+        var _a, _b;
         const roomId = userIdToSocket.get(socket.id);
+        if (!allusers[roomId]) {
+            return;
+        }
+        const username = (_a = allusers[roomId].find(user => user.id === socket.id)) === null || _a === void 0 ? void 0 : _a.username;
+        const to = (_b = allusers[roomId].find(user => user.username !== username)) === null || _b === void 0 ? void 0 : _b.id;
         if (roomId && allusers[roomId]) {
             const index = allusers[roomId].findIndex(user => user.id === socket.id);
             if (index !== -1) {
-                const [user] = allusers[roomId].splice(index, 1);
-                console.log(`User ${user.username} left room: ${roomId}`);
-                // socket.to(roomId).emit('user-left', { username: user.username, id: socket.id });
+                allusers[roomId].splice(index, 1);
+                socket.leave(roomId);
+                userIdToSocket.delete(socket.id);
+                if (to) {
+                    socket.to(to).emit('user-left', { username, id: socket.id });
+                }
             }
             if (allusers[roomId].length === 0) {
                 delete allusers[roomId];
             }
         }
         userIdToSocket.delete(socket.id);
-        console.log(allusers[roomId], " ", userIdToSocket);
-        // allusers.forEach((users, roomId) => {
-        //     const index = users.findIndex(user => user.id === socket.id);
-        //     if (index !== -1) {
-        //         const [user] = users.splice(index, 1);
-        //         console.log(`User ${user.username} left room: ${roomId}`);
-        //         socket.to(roomId).emit('user-left', { username: user.username, id: socket.id });
-        //     }
-        // }
     });
 });
